437306969a305f666b0703aa72e37a843fa3f95c
oyeniyi kehinde <kehindeoyeniyi08@gmail.com>
2025-12-03 08:48:35 -0800

    changed allowed host settings

--- project/settings.py
+++ project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 # Application definition
 
